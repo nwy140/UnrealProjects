981fe89c573bb78a03aa0329c8a7601b5ebf8b90
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2017-11-13 19:58:07 +0800

    Unreal Engine Startup Project

---
+++ Section_01/Intermediate/Build/Win64/UE4Editor/Inc/Section_01/Section_01.init.gen.cpp
@@ -0,0 +1,36 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+PRAGMA_DISABLE_OPTIMIZATION
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeSection_01_init() {}
+	UPackage* Z_Construct_UPackage__Script_Section_01()
+	{
+		static UPackage* ReturnPackage = nullptr;
+		if (!ReturnPackage)
+		{
+			ReturnPackage = CastChecked<UPackage>(StaticFindObjectFast(UPackage::StaticClass(), nullptr, FName(TEXT("/Script/Section_01")), false, false));
+			ReturnPackage->SetPackageFlags(PKG_CompiledIn | 0x00000000);
+			FGuid Guid;
+			Guid.A = 0x57CE750E;
+			Guid.B = 0xABBB4B3E;
+			Guid.C = 0x00000000;
+			Guid.D = 0x00000000;
+			ReturnPackage->SetGuid(Guid);
+
+		}
+		return ReturnPackage;
+	}
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
+PRAGMA_ENABLE_OPTIMIZATION
