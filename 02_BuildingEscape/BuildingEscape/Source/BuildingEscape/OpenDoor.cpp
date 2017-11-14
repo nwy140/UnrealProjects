f92c6cbd2969c7544d8249ca1cb02b4cd93f36a4
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2017-11-14 10:31:05 +0800

    Created OpenDoor Componenet that SetActorRotation(NewActorRotation) to rotate and open door, FRotater NewRotation is set using FRotator constructor and passing coordinate parameters

---
+++ 02_BuildingEscape/BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,46 @@
+// Copyright Ng Wei Yuen
+
+#include "OpenDoor.h"
+#include <GameFramework/Actor.h>
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Find Owning Actor
+	AActor *Owner = GetOwner();
+
+	//Create a Rotator
+	FRotator NewRotation = FRotator(0.f,-60.f,0.f);// FRotater Constructor Pass X,Y,Z as pitch , yaw and roll
+															// pitch is x axis up and down, Yaw is ground left and right, Roll is z axis up and down
+	//Set the door Rotation
+	Owner->SetActorRotation(NewRotation);
+
+
+	//UE_LOG(LogTemp, Error, TEXT("%s"), Rotater.ToString());
+
+	// ...
+
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
