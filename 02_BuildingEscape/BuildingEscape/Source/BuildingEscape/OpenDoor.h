c15e60aac5c345ae21a0a618065b28eaef81c54c
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2017-11-14 16:27:50 +0800

    Macro UPROPERTY is used to show variables on DetailsTab of UEditor , (VisibleAnywhere) or (EditAnywhere) an be passed in UPROPERTY() macro, TriggerVolume can be selected for PressurePlate at details tab

--- 02_BuildingEscape/BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ 02_BuildingEscape/BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -4,15 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
-
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	// Sets default values for this component's properties
 	UOpenDoor();
 
@@ -20,10 +20,20 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
-	
+private:
+	UPROPERTY(VisibleAnywhere)
+	float OpenAngle = 90.0f; //shows a non editable OpenAngle Property in details in UEEditor
+						 // this macro does a strange cut and paste in our code before it actually compiles // macros don't need semi colon
+						 //macro parameter has no relevant auto complete because visual studio does not know what is going on for macros
+						 //make sure there is no space after UProperty
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate; // TriggerVolume pointer shows up at details via macro , and can be edited	
+									//UPROPERTY macro shows PressurePlate in details tab, click it, then select TriggerVolume object to associate with pressureplate
+								   // to see what type an object is, drag it to world outliner,  see its type, then include its header and use intelisense to type its type in c++
+					
 };
