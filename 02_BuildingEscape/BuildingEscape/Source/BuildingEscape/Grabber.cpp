74dc04bb1eaeece6a651a91a5b734008b77d36d3
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2017-11-15 20:12:12 +0800

    Added few lines of code to Print PhysicsBody Object to console when hit , you have to make sure that pointers does not points to NULL or else Unreal Engine will crash

--- 02_BuildingEscape/BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ 02_BuildingEscape/BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -1,4 +1,4 @@
-// Copyright Ng Wei Yuen
+/// Copyright Ng Wei Yuen
 
 #include "Grabber.h"
 #include "GameFramework/Actor.h"
@@ -9,62 +9,80 @@
 
 #define OUT
 
-// Sets default values for this component's properties
+/// Sets default values for this component's properties
 UGrabber::UGrabber()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
+	/// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	/// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+	/// ...
 }
 
 
-// Called when the game starts
+/// Called when the game starts
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
 	UE_LOG(LogTemp, Warning, TEXT("Grabber %s reporting for duty!"), *(GetOwner()->GetName()));
 
-	// ...
+	/// ...
 
 }
 
 
-// Called every frame
+/// Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	//Get player view point this tick
+	///Get player view point this tick
 
 	FVector PlayerViewPointLocation;
 	FRotator PlayerViewPointRotation;
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
-		OUT	PlayerViewPointLocation, //Set ViewPointLocation
-		OUT	PlayerViewPointRotation //OUT actually does nothing /// Just marking parameters with OUT to show the variables you need to log out
-	); //Go from world, then get player, then get view of player
+		OUT	PlayerViewPointLocation, ///Set ViewPointLocation
+		OUT	PlayerViewPointRotation ///OUT actually does nothing //// Just marking parameters with OUT to show the variables you need to log out
+	); ///Go from world, then get player, then get view of player
 
 
-	//TODO: LOG out to test
-	//UE_LOG(LogTemp, Warning, TEXT("Location : %s , Rotation : %s "), *(PlayerViewPointLocation.ToString()), *(PlayerViewPointRotation.ToString()));
+	///TODO: LOG out to test
+	///UE_LOG(LogTemp, Warning, TEXT("Location : %s , Rotation : %s "), *(PlayerViewPointLocation.ToString()), *(PlayerViewPointRotation.ToString()));
 
-	FVector LineTraceEnd = PlayerViewPointLocation  + PlayerViewPointRotation.Vector() * Reach;
-	// Draw a red trace in the world to visualize
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+	/// Draw a red trace in the world to visualize
 	DrawDebugLine(
 		GetWorld(),
 		PlayerViewPointLocation,
 		LineTraceEnd,
 		FColor(255, 0, 0),
-		false, // won't disapper
+		false, /// won't disapper
 		0.f,
 		0.f,
 		10.f
 	);
 
-	//Ray-cast out to reach distance  // imagine superman laser eyes logging
 
-	//See what we hit
+	///Setup query parameter
+	FCollisionQueryParams	TraceParameters(FName(TEXT("")), false, GetOwner());
+
+	/// Line-trace (AKA Ray-cast) out to reach distance  /// imagine superman laser eyes logging
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType( //Detect Object hit 
+		OUT Hit, /// mark it as OUT parameter
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+
+	); ////LineTraceMulti will pass through multiple objects and report multiple answers	
+
+	///See what we hit
+	AActor * ActorHit = Hit.GetActor();
+	if (ActorHit) // Since this loads every frame, it will crash before hit is instantiated because it points to nullpointer, so you need to prevent this error with an if statement
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *(ActorHit->GetName() ));
+	}
 }
 
