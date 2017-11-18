628e8f7bd47122a1dd195b3b2c0e71cbff4f9835
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2017-11-18 15:21:48 +0800

    Used GrabComponentWithRotation() method to grab objects with physics body and ReleaseComponent to release objects when Grab Action released, you need to #include "Components/PrimitiveComponent.h" to access methods inside component attached to other objects other than your own to use methods like GetOwner()->GetActorLocation()

--- 02_BuildingEscape/BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ 02_BuildingEscape/BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -6,6 +6,9 @@
 #include "PhysicsEngine/PhysicsHandleComponent.h"
 
 #include "Engine/World.h"
+#include "Components/PrimitiveComponent.h"
+
+
 
 #include "DrawDebugHelpers.h"
 
@@ -41,14 +44,26 @@ void UGrabber::Grab() {
 	//Avoid using semicolons at the end of macros to avoid bugs in the future
 	UE_LOG(LogTemp, Warning, TEXT("Grab pressed"))
 		// LINE TRACE and see if we reach any actors with physicsbody collision channel set
-		GetFirstPhysicBodyInReach();
+	auto HitResult = GetFirstPhysicBodyInReach(); //auto is a c++ 11 type that automatically decides an appropriate type for you
+	auto ComponentToGrab = HitResult.GetComponent();
+	auto ActorHit = HitResult.GetActor();
+
 	// if we hit something then attach a physic handle
-	//TODO attach physic handle	
+	if (ActorHit) { // if actor hit not equal null
+		//TODO attach physic handle	
+		PhysicHandle->GrabComponentAtLocationWithRotation( //Grab Component method no longer work with new API, so use GrabcomponentwithLocation
+			ComponentToGrab,
+			NAME_None,
+			ComponentToGrab->GetOwner()->GetActorLocation(),  // must #include "Components/PrimitiveComponent.h" or else you can't refer to other object components other then the object with current class attached
+			FRotator(0) //allow rotation  // true doesnot work with new API, use FROTATOR(0) instead
+		);
+	}
 }	
 
 void UGrabber::Release() {
 	UE_LOG(LogTemp, Warning, TEXT("Grab released"))
-	//TODO release physic handle
+		//TODO release physic handle
+		PhysicHandle->ReleaseComponent();
 }
 #pragma endregion 
 
@@ -87,12 +102,30 @@ void UGrabber::SetupInputComponent()
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+#pragma region tmp
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT	PlayerViewPointLocation, ///Set ViewPointLocation
+		OUT	PlayerViewPointRotation ///OUT actually does nothing //// Just marking parameters with OUT to show the variables you need to log out
+	); ///Go from world, then get player, then get view of player
 
+
+	   ///TODO: LOG out to test
+	   ///UE_LOG(LogTemp, Warning, TEXT("Location : %s , Rotation : %s "), *(PlayerViewPointLocation.ToString()), *(PlayerViewPointRotation.ToString()));
+
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+#pragma endregion 
 	// if physics handle is attached
+	if (PhysicHandle->GrabbedComponent){
+		
 		// move the object we are handling	
-
+		PhysicHandle->SetTargetLocation(LineTraceEnd);
+	}
+	
 
 }
+
 
 
 const FHitResult UGrabber::GetFirstPhysicBodyInReach()
@@ -142,6 +175,6 @@ const FHitResult UGrabber::GetFirstPhysicBodyInReach()
 	{
 		UE_LOG(LogTemp, Warning, TEXT("Line Trace hit: %s"), *(ActorHit->GetName()));
 	}
-	return FHitResult();
+	return Hit;
 }
 
