7dfb4b54055a36a88ba0e0f4ce9365c35952886f
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2017-11-28 13:52:21 +0800

    Get Screen Pixel Coordinates by using GetViewPortSize(X,Y) method, then FVector2D ScreenLocation = FVector2D (X*CrossHairXLocation,Y*CrossHairYLocation);

--- 03_BattleTank/BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ 03_BattleTank/BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -55,12 +55,11 @@ void ATankPlayerController::Tick(float deltatime)
 void ATankPlayerController::AimTowardsCrosshair()
 
 {
-
 	if (!GetControlledTank()) { return; }
 	FVector HitLocation; //OUT parameter
 
 	if (GetSightRayHitLocation(HitLocation)) { //Has "side-effect" , is going to line trace
-		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+		//UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
 
 		//if hits the lanscape
 
@@ -71,6 +70,18 @@ void ATankPlayerController::AimTowardsCrosshair()
 // GetWorldLocation of linetrace through crosshair	 , true if hit linescape		
 bool ATankPlayerController::GetSightRayHitLocation(FVector & OutHitLocation) const
 {
-	OutHitLocation = FVector(1, 0,1);
+#pragma region Find crosshair position
+	// Find the crosshair position
+
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+
+	auto ScreenLocation = FVector2D(ViewportSizeX*CrossHairXLocation, ViewportSizeY* CrossHairYLocation); /// auto is a new C++11 type that automatically sets the correct class type for you
+	//UE_LOG(LogTemp, Warning, TEXT("ScreenLocation: %s"), *ScreenLocation.ToString())
+#pragma endregion
+
+	// "De-project" the screen position of the crosshair to a world direction
+		// Line-trace alog that look direction, and see what we hit (up to max range)
+		OutHitLocation = FVector(1, 0, 1);
 	return true;
 }
--- 03_BattleTank/BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ 03_BattleTank/BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -34,22 +34,35 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 
 public:
 
-	ATank* GetControlledTank() const;
-
-	void BeginPlay() override;
-
-
-
-	virtual void Tick(float deltatime) override;
 
 
 
 private:
+#pragma region DeclareVariables 
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = 0.5;   /// crosshair 50% across screen
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = 0.33333; /// cross hair 1/9 across screen
+#pragma endregion
 
-	//start the tank moving the barrel so that a shot would hit where the crosshair  intersects the world
-	
+#pragma region DeclarePointers
+	///You got to include headers for every class component you are using
+	///Eg :	UInputComponent * InputComponent = nullptr;
+#pragma endregion
+
+#pragma region DeclareMethods
+	ATank* GetControlledTank() const;
 	void AimTowardsCrosshair();
-	bool GetSightRayHitLocation(FVector& OutHitLocation) const; 
+	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+	//start the tank moving the barrel so that a shot would hit where the crosshair  intersects the world
+#pragma region EngineMethodsoverride
+	void BeginPlay() override;
 
+	virtual void Tick(float deltatime) override;
+#pragma endregion
+#pragma endregion
+
+
+	
 
 };
