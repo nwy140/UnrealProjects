d261a69c9fde9d6354bdb310476d2745104f1b10
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2017-12-28 22:59:21 +0800

    Created Blank StaterContent Project, then migrate Explosion particle from StarterContent to BattleTank Project. Renamed explosion particle to LaunchBlast Then , duplicate emmiter of Smoke, and name it smoke trail. Then set SmokeTrail's lifeovertime, colorovertime to be float constant. Disable local space in Required . In Projectile_BP, Set LaunchBlast staticmesh's particle to LaunchBlast particle

--- 03_BattleTank/BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ 03_BattleTank/BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -1,48 +1,48 @@
-//// Fill out your copyright notice in the Description page of Project Settings.
-//
-//#include "Projectile.h"
-//#include "GameFramework/ProjectileMovementComponent.h"
-//#include "Particles/ParticleSystemComponent.h"
-//#include "Engine/StaticMesh.h"
-//
-//
-//// Sets default values
-//AProjectile::AProjectile()
-//{
-//	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-//	PrimaryActorTick.bCanEverTick = true;
-//
-//	CollisionMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Collision Mesh"));
-//	SetRootComponent(CollisionMesh); 
-//	CollisionMesh->SetNotifyRigidBodyCollision(true);
-//	CollisionMesh->SetVisibility(true);
-//
-//	LaunchBlast = CreateDefaultSubobject<UParticleSystemComponent>(FName("Launch Blast"));
-//	LaunchBlast->AttachTo(RootComponent);
-//	
-//	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile Movement")); //Create UProjectileMovement component to be added to class which will be inherited to Projectile_BP
-//	ProjectileMovement->bAutoActivate = false; //this means it won't stop flying off until we actually fire it
-//
-//}
-//
-//// Called when the game starts or when spawned
-//void AProjectile::BeginPlay()
-//{
-//	Super::BeginPlay();
-//
-//}
-//
-//
-//// Called every frame
-//void AProjectile::Tick(float DeltaTime)
-//{
-//	Super::Tick(DeltaTime);
-//
-//}
-//
-//void AProjectile::LaunchProjectile(float speed)
-//{
-//
-//	ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector * speed);//This adds velocity to Projectile // ForwardVector is barrel's forward vector hopefully		
-//	ProjectileMovement->Activate();
-//}
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Projectile.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Engine/StaticMesh.h"
+
+
+// Sets default values
+AProjectile::AProjectile()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Collision Mesh"));
+	SetRootComponent(CollisionMesh); 
+	CollisionMesh->SetNotifyRigidBodyCollision(true);
+	CollisionMesh->SetVisibility(true);
+
+	LaunchBlast = CreateDefaultSubobject<UParticleSystemComponent>(FName("Launch Blast"));
+	LaunchBlast->AttachTo(RootComponent);
+	
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile Movement")); //Create UProjectileMovement component to be added to class which will be inherited to Projectile_BP
+	ProjectileMovement->bAutoActivate = false; //this means it won't stop flying off until we actually fire it
+
+}
+
+// Called when the game starts or when spawned
+void AProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+
+// Called every frame
+void AProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AProjectile::LaunchProjectile(float speed)
+{
+
+	ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector * speed);//This adds velocity to Projectile // ForwardVector is barrel's forward vector hopefully		
+	ProjectileMovement->Activate();
+}
